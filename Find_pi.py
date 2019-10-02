62e2c97996fbc4408515c267402d5ba7987e68c9
sagarapatel <38914967+sagarapatel@users.noreply.github.com>
2019-10-03 02:03:38 +1300

    Add files via upload

---
+++ Find_pi.py
@@ -0,0 +1,26 @@
+
+# Find PI to the Nth Digit
+# Program generate PI with decimal places up to value of n
+# Limit upto 400 decimal places.
+# Ref: https://www.math.hmc.edu/funfacts/ffiles/20010.5.shtml
+
+from __future__ import division
+import math
+import decimal
+from decimal import Decimal
+from decimal import getcontext
+
+
+def compute_pi(n):
+    maximum = 10000
+    decimal.getcontext().prec = n + 1
+    pi = Decimal(0)
+
+    for k in range(maximum):
+        pi += Decimal(math.pow(16, -k)) * (Decimal(4 / (8 * k + 1)) - Decimal(2 / (8 * k + 4)) -
+                                           Decimal(1 / (8 * k + 5)) - Decimal(1 / (8 * k + 6)))
+    return pi
+
+
+n = 100
+print(compute_pi(n))
