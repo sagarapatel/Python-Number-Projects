8c3197f5cf74c635cff24511bfd7366aab12afe3
sagarapatel <38914967+sagarapatel@users.noreply.github.com>
2019-10-05 03:12:39 +1300

    Add path for github

--- Find_pi.py
+++ Find_pi.py
@@ -3,6 +3,7 @@
 # Program generate PI with decimal places up to value of n
 # Limit upto 400 decimal places.
 # Ref: https://www.math.hmc.edu/funfacts/ffiles/20010.5.shtml
+# Github: https://github.com/sagarapatel/Python-Number-Projects/blob/master/Find_pi.py
 
 from __future__ import division
 import math
