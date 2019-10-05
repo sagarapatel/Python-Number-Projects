5b417c3b883bd31d4ea9a59140c4f854bfd177a7
Sagar Patel <sagarapatel@gmail.com>
2019-10-05 14:15:38 +1300

    Add version details

--- Find_pi.py
+++ Find_pi.py
@@ -4,6 +4,8 @@
 # Limit upto 400 decimal places.
 # Ref: https://www.math.hmc.edu/funfacts/ffiles/20010.5.shtml
 # Github: https://github.com/sagarapatel/Python-Number-Projects/blob/master/Find_pi.py
+# IDE: PyCharm
+# Version: Python compatible 3.5
 
 from __future__ import division
 import math
