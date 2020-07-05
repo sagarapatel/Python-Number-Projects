221a649373ec9fa03abb882fd4c041f5559a6c59
Sagar Patel <sagarapatel@gmail.com>
2020-07-06 01:47:47 +1000

    Final version fixed user input issue from previous version.

--- prime_number.py
+++ prime_number.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-# Find prime numbers until user ask
+# Generate prime numbers until user wants
 # https://github.com/sagarapatel/Python-Number-Projects/blob/master/prime_number.py
 # IDE PyCharm
 # Python 3.5 compatible
 
+n = 'y'
 
-def prime_number():
+
+# Function generate prime numbers individually until user wants
+def prime_number(n):
     for i in range(2, 100):
-        n = input("Enter 'y' for next prime number or Any other key to Exit ")
         if n == 'y' or n == 'Y':
             for num in range(2, i):
                 if (i % num) == 0:
                     break
             else:
                 print(str(i) + " is a prime number")
+                n = input("Enter 'y' for next prime number or Any other key to Exit ")
         else:
             print("Thank you")
             break
 
 
-prime_number()
+prime_number(n)
