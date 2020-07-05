e4b5af5b7cc07ae2d7194d10b48220dd0db3d4b4
Sagar Patel <sagarapatel@gmail.com>
2020-07-05 21:58:34 +1000

    Final version of Fibonacci Sequence program.

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -4,9 +4,9 @@
 # Final Github path
 # IDE: PyCharm
 # Version: Python 3.5 compatible
+
+
 # Function to calculate fibonacci value
-
-
 def compute_fibo(n):
     n1, n2 = 0, 1
     count = 0
@@ -18,9 +18,10 @@ def compute_fibo(n):
         count += 1
 
 
+# Ask for user input and handle Value Error
 while True:
     try:
-        value = input("Please enter number up to fibonacci_sequence ")
+        value = input("Please enter number up to fibonacci_sequence calculate ")
         n = int(value)
         compute_fibo(n)
         break
