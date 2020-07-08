4a11961376b39c16c079692ece9763f4f1baa462
Sagar Patel <sagarapatel@gmail.com>
2020-07-09 07:39:02 +1000

    Handle Exceptions and Errors

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -7,19 +7,24 @@
 
 from math import pow
 
-print("Welcome to the Mortgage Calculator ")
-amount = int(input("Enter the amount of Mortgage "))
-interest = float(input("Enter mortgage interest rate "))
-years = int(input("Enter mortgage years "))
-repayments = input("Enter 'm' for Monthly repayments, 'f' for fortnightly repayments ")
-if repayments == 'm' or repayments == 'M':
-    interest_rate = (interest / 12) / 100
-    time = years * 12
-    n = 'Monthly'
-elif repayments == 'f' or repayments == 'F':
-    interest_rate = (interest / 26) / 100
-    time = years * 26
-    n = 'Fortnightly'
-
-loan_payment = amount * ((interest_rate * pow((1 + interest_rate), time)) / (pow((1 + interest_rate), time) - 1))
-print("Your {first} loan payment will be ${payment}".format(first=n, payment=round(loan_payment, 2)))
+try:
+    print("Welcome to the Mortgage Calculator ")
+    amount = int(input("Enter the amount of Mortgage "))
+    interest = float(input("Enter mortgage interest rate "))
+    years = int(input("Enter mortgage years "))
+    repayments = input("Enter 'm' for Monthly repayments, 'f' for fortnightly repayments ")
+    if repayments == 'm' or repayments == 'M':
+        interest_rate = (interest / 12) / 100
+        time = years * 12
+        n = 'Monthly'
+    elif repayments == 'f' or repayments == 'F':
+        interest_rate = (interest / 26) / 100
+        time = years * 26
+        n = 'Fortnightly'
+    else:
+        raise Exception("Invalid Input.")
+except ValueError:
+    print("Please Enter Correct Value")
+else:
+    loan_payment = amount * ((interest_rate * pow((1 + interest_rate), time)) / (pow((1 + interest_rate), time) - 1))
+    print("Your {first} loan payment will be ${payment}".format(first=n, payment=round(loan_payment, 2)))
