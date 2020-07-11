16357b4a1838136cae58d4702d5c022d99c8a9a1
Sagar Patel <sagarapatel@gmail.com>
2020-07-11 11:52:00 +1000

    Seperate Dollars and Cents and display cents in two decimal points.

--- change_return.py
+++ change_return.py
@@ -5,49 +5,51 @@
 # Python 3.8 compatible
 
 cost = float(input("Please enter the cost of the product "))
-money = float(input("Please enter money given by customer "))
-change = money - cost
+received = float(input("Please enter money given by customer "))
+change = received - cost
 
 
-def compute(change, currency):
-    quantity = change / currency
-    remainder = change % currency
-    change = remainder
-    print(str(int(quantity)) + " Note(s) of {}".format(currency))
-    return round(change, 2)
+def compute(amount, currency, type):
+    quantity = amount / currency
+    remainder = amount % currency
+    if currency < 1:
+        print(str(int(quantity)) + " {} of ${:.2f} Cent".format(type, currency))
+    else:
+        print(str(int(quantity)) + " {} of ${} Dollar".format(type, currency))
+    return round(remainder, 2)
 
 
-if change > 100:
-    change = compute(change, 100)
+if change >= 100:
+    change = compute(change, 100, 'Note(s)')
 
-if change > 50:
-    change = compute(change, 50)
+if change >= 50:
+    change = compute(change, 50, 'Note(s)')
 
-if change > 20:
-    change = compute(change, 20)
+if change >= 20:
+    change = compute(change, 20, 'Note(s)')
 
-if change > 10:
-    change = compute(change, 10)
+if change >= 10:
+    change = compute(change, 10, 'Note(s)')
 
-if change > 5:
-    change = compute(change, 5)
+if change >= 5:
+    change = compute(change, 5,  'Note(s)')
 
-if change > 2:
-    change = compute(change, 2)
+if change >= 2:
+    change = compute(change, 2, 'Coin(s)')
 
-if change > 1:
-    change = compute(change, 1)
+if change >= 1:
+    change = compute(change, 1, 'Coin(s)')
 
-if change > 0.50:
-    change = compute(change, 0.50)
+if change >= 0.50:
+    change = compute(change, 0.50, 'Coin(s)')
 
-if change > 0.20:
-    change = compute(change, 0.20)
+if change >= 0.20:
+    change = compute(change, 0.20, 'Coin(s)')
 
-if change > 0.10:
-    change = compute(change, 0.10)
+if change >= 0.10:
+    change = compute(change, 0.10, 'Coin(s)')
 
-if change > 0.05:
-    change = compute(change, 0.05)
+if change >= 0.05:
+    change = compute(change, 0.05, 'Coin(s)')
 
 print(change)
