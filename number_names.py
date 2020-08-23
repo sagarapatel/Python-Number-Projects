68e01b0ce0f612632fb576572f1e86feae28d3ec
Sagar Patel <sagarapatel@gmail.com>
2020-08-23 21:02:30 +1000

    Final Version of the program all working.

--- number_names.py
+++ number_names.py
@@ -4,6 +4,7 @@
 # IDE PyCharm
 # Python 3.8 compatible
 
+# Dictionary specify names to numbers
 ones = {
     1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"
 }
@@ -16,20 +17,30 @@ count = {
     3: "hundred", 4: "thousand", 6: "hundred"
 }
 
-x = 17221
+# User input integer number
+x = int(input("Please enter positive number to convert into words "))
 length = len(str(x))
+skip = 0
+# Iterate through the numbers
 for n in str(x):
     digit = int(n)
-    if digit in ones and length in (3, 4, 6) and digit != 0:
-        print(ones[digit])
-    if (length in count and digit in ones and length in (3, 4, 5, 6) and digit != 0) or (length == 4 and digit == 0):
-        print(count[length])
+    # IF block for ones digit
+    if digit in ones and length in (1, 3, 4, 6) and digit != 0 and skip == 0:
+        print(ones[digit], end=" ")
+    # IF block for words between numbers
+    if (length in count and length in (3, 4, 5, 6) and digit != 0) or (length == 4 and digit == 0):
+        print(count[length], end=" ")
+        skip = 0
+    # IF block for numbers in tens
     if length in (2, 5) and digit != 0:
         if digit == 1 and length == 2:
             digit = abs(x) % 100
-            length -= 2
-        print(tens[digit])
-    if length == 1 and digit != 0:
-        print(ones[digit])
+            skip = 1
+        elif digit == 1 and length == 5:
+            digit = str(x)[-5:-3]
+            skip = 1
+        print(tens[int(digit)], end=" ")
+    # IF block for number zero
+    if length == 1 and x == 0:
+        print("Zero")
     length -= 1
-print(x)
